bc78bd4da07152624f1f47cfb42aa82ef9db5893
mdeshpa <milind_deshpande@persistent.com>
2017-02-09 16:44:48 -0500

    Added scenarios for Saerch and Login through FB
    Added scenarios for Saerch and Login through FB

--- NightWatch/tests/frontdoorsearch/frontdoorsearch.js
+++ NightWatch/tests/frontdoorsearch/frontdoorsearch.js
@@ -9,15 +9,17 @@ module.exports = {
 // Search for content containing word America		      
 		      
 			  .setValue("//*[@id='search']/input", ['America', client.Keys.ENTER])
-			  .waitForElementVisible("//*[@id='main']/div/div/div/div[2]/div/div/div/section/div[1]/article/figure/div/div/header/h2/a/span", 50000)
-
-			  .verify.containsText("//*[@id='main']/div/div/div/div[2]/div/div/div/section/div[1]/article/figure/div/div/header/h2/a/span", 'What Great America...?')
-			  .verify.containsText("//*[@id='main']/div/div/div/div[2]/div/div/div/section/div[2]/article/figure/div/div/header/h2/a/span", 'A Prayer For America')
-		      
-		  	  .verify.urlContains("https://www.theodysseyonline.com/search-results?query=America")   
-			 
-// School search containing word IUPUI				  	  
-		  	  .clearValue("//*[@id='search']/input")
+			  .waitForElementVisible("//*[@id='main']/div/div/div/div[2]/div/div/div/section/div[1]/article/figure/div/div/header/h2/a/span", 50000)		      
+		  	  .verify.urlContains("https://www.theodysseyonline.com/search-results?query=America") 
+		  	  
+		  	  .click("//*[@id='main']/div/div/div/div[2]/div/div/div/section/div[1]/article/figure/div/div/header/h2/a/span")
+		  	  .waitForElementVisible("//*[@id='full-article-body']", 50000) 
+		  	  .assert.containsText("//*[@id='full-article-body']", "America")		  	  
+		  	  .assert.containsText("//*[@id='fullArticle']/figure/div/header", "America")
+		  	  .assert.containsText("//*[@id='fullArticle']/figure/div/header", "America")
+ 
+// School search containing word IUPUI		
+		  	  .click("//*[@id='app-bar-search-menu']")
 
 		  	  .setValue("//*[@id='search']/input", ['IUPUI', client.Keys.ENTER])
 			  .waitForElementVisible("//*[@id='main']/div/div/div/div[2]/div/div/div/section/div[1]/article/figure/div/div/header/a[1]/span", 50000)
@@ -27,9 +29,6 @@ module.exports = {
 		      
 		  	  .verify.urlContains("https://www.theodysseyonline.com/search-results?query=IUPUI")   
 		  	  
-		  	  
-		  	  
-		  	  
-//		  	  .end();	  	  	  
+		  	  .end();	  	  	  
 		}
 };
